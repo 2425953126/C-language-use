b2cfdfb50f20ed3d4c65ed00d51093ff3f7b147b
谢明轩 <2425953126@qq.com>
2022-10-27 13:35:23 +0000

    offsetof的宏模拟实现 优雅，永不过时       2022/10/27
    Signed-off-by: 谢明轩 <2425953126@qq.com>

---
+++ 日常练习/offset宏实现.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#define My_offsetof(x,stu)	int count=0;							\
+							while(count<sizeof(*stu)&&stu!=&x)		\
+							{										\
+								count++;							\
+								stu = (char*)stu + 1;				\
+							}										\
+							printf(#x"成员首地址偏移为%d\n",count);	\
+
+int main()
+{
+	struct test
+	{
+		int a;
+		char s[10];
+		float b;
+		int c;
+	}new={13,"abcde",3.14,20};
+	struct test* p = &new;
+	My_offsetof(new.b, p)
+	return 0;
+}
