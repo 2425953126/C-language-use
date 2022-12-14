c2ea50d4e0da60e12853533af6957608b0367461
谢明轩 <2425953126@qq.com>
2022-12-14 15:19:32 +0000

    修复了一个扩容函数的bug
    Signed-off-by: 谢明轩 <2425953126@qq.com>

--- 数据结构C/支持动态增长的栈/STACK.C
+++ 数据结构C/支持动态增长的栈/STACK.C
@@ -1,14 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "STACK.h"
-// ��ʼ��ջ 
+// 初始化栈 
 void StackInit(Stack* ps)
 {	
 	assert(ps);
 	ps->a = NULL;
 	ps->capacity = 0;
-	ps->top = -1;//��-1Ϊ��,��ջ���ȼ�һ,�ٴ�����
+	ps->top = -1;//以-1为底,即栈顶先加一,再存数据
 }
-// ����ջ 
+// 销毁栈 
 void StackDestroy(Stack* ps)
 {	
 	assert(ps);
@@ -17,12 +17,12 @@ void StackDestroy(Stack* ps)
 	ps->capacity = 0;
 	ps->top = -1;
 }
-//���ݺ���,ÿ������Ϊԭ������1.25��+4����Ա��ռ�ռ�
+//扩容函数,每次扩容为原容量的1.25倍+4个成员所占空间
 void ExpanSpace(Stack*ps)
 {
 	STDataType* tmp=NULL;
-	tmp = realloc(tmp,(int)(sizeof(STDataType)*ps->capacity*1.25+4*sizeof(STDataType)));//4*sizeof(STDataType)��jb���������ӵ���һСʱ,
-																						//������ֻ+4����ֻ����һ���ֽڿռ�,��һ��int�Ϳռ�
+	tmp = realloc(tmp,(int)(sizeof(STDataType)*ps->capacity*1.25+4*sizeof(STDataType)));//4*sizeof(STDataType)吊jb东西害老子调错一小时,
+																						//这边如果只+4代表只申请一个字节空间,即一个int型空间
 	if (tmp == NULL)
 	{	
 		perror("ExpanSpace");
@@ -30,36 +30,36 @@ void ExpanSpace(Stack*ps)
 	}
 	else
 		ps->a = tmp;
-	ps->capacity = (int)(sizeof(STDataType) * ps->capacity *1.25 + 4);
+	ps->capacity = (int)( ps->capacity *1.25 + 4);
 }
-// ��ջ 
+// 入栈 
 void StackPush(Stack* ps, STDataType data)
 {
 	assert(ps);
-	if (ps->top == (ps->capacity - 1))//��������,������
+	if (ps->top == (ps->capacity - 1))//容量不足,则扩容
 		ExpanSpace(ps);
-	ps->top++;//���Լ�,�ٴ�����
+	ps->top++;//先自加,再存数据
 	ps->a[ps->top] = data;
 }
-// ��ջ 
+// 出栈 
 void StackPop(Stack* ps)
 {
-	assert(ps && ps->top != -1);//���ջָ���Ƿ�ΪNULL,��ջ�ڳ�Ա����Ϊ��
+	assert(ps && ps->top != -1);//检测栈指针是否为NULL,且栈内成员不得为无
 	ps->top--;
 }
-// ��ȡջ��Ԫ�� 
+// 获取栈顶元素 
 STDataType StackTop(Stack* ps)
 {
 	assert(ps && ps->top != -1);
 	return ps->a[ps->top]; 
 }
-// ��ȡջ����ЧԪ�ظ��� 
+// 获取栈中有效元素个数 
 int StackSize(Stack* ps)
 {
 	assert(ps);
 	return ps->top + 1;
 }
-// ���ջ�Ƿ�Ϊ�գ����Ϊ�շ��ط������������Ϊ�շ���0 
+// 检测栈是否为空，如果为空返回非零结果，如果不为空返回0 
 int StackEmpty(Stack* ps)
 {
 	assert(ps);
